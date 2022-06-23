801befa269e884c029ae60e0c8e389697b99da32
khaled39ab <khaled39ab@gmail.com>
2022-06-23 22:26:16 +0600

    created header component

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import CustomLink from '../Others/CustomLink/CustomLink';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            {/* <img src={logo} alt="" /> */}
+            <div className='custom-link'>
+                <CustomLink to={'/'}>Home</CustomLink>
+                <CustomLink to={'/shop'}>Shop</CustomLink>
+                <CustomLink to={'/order-review'}>Order Review</CustomLink>
+                <CustomLink to={'/inventory'}>Inventory</CustomLink>
+                <CustomLink to={'/about'}>About</CustomLink>
+                <CustomLink to={'/login'}>Log In</CustomLink>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
