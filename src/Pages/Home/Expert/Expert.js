ebc9aaf4bd7e612f8c2e5aa25b46e2c8cdeb7d8a
khaled39ab <khaled39ab@gmail.com>
2022-06-24 19:04:31 +0600

    design services component and responsive with media query

---
+++ src/Pages/Home/Expert/Expert.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Expert = ({ expert }) => {
+    const { name, img } = expert;
+
+    return (
+        <div className='col-sm-12 col-md-6 col-lg-4'>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={img} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default Expert;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import CustomLink from '../../Others/CustomLink/CustomLink';
+import logo from '../../../images/logo-black.png'
 import './Header.css'
 
 const Header = () => {
     return (
         <nav className='header'>
-            {/* <img src={logo} alt="" /> */}
+            <img src={logo} alt="" />
             <div className='custom-link'>
                 <CustomLink to={'/'}>Home</CustomLink>
-                <CustomLink to={'/shop'}>Shop</CustomLink>
-                <CustomLink to={'/order-review'}>Order Review</CustomLink>
+                <CustomLink to={'/booking-review'}>Booking Review</CustomLink>
                 <CustomLink to={'/inventory'}>Inventory</CustomLink>
                 <CustomLink to={'/about'}>About</CustomLink>
                 <CustomLink to={'/login'}>Log In</CustomLink>
