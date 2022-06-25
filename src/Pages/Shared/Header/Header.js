1e6971a05ecf527a1a747e94af0b1c21efb4a769
khaled39ab <khaled39ab@gmail.com>
2022-06-25 15:31:27 +0600

    make header responsive with react bootstrap

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import CustomLink from '../../Others/CustomLink/CustomLink';
 import logo from '../../../images/logo-black.png'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import './Header.css'
 
 const Header = () => {
     return (
-        <nav className='header'>
-            <img src={logo} alt="" />
-            <div className='custom-link'>
-                <CustomLink to={'/'}>Home</CustomLink>
-                <CustomLink to={'/booking-review'}>Booking Review</CustomLink>
-                <CustomLink to={'/inventory'}>Inventory</CustomLink>
-                <CustomLink to={'/about'}>About</CustomLink>
-                <CustomLink to={'/login'}>Log In</CustomLink>
-
-            </div>
-        </nav>
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home" className='brand-icon'>
+                    <img src={logo} alt="" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                    </Nav>
+                    <Nav className='link-name'>
+                        <Nav.Link href="#features">Services</Nav.Link>
+                        <Nav.Link href="#pricing">Experts</Nav.Link>
+                        <Nav.Link href="#dee">About</Nav.Link>
+                        <Nav.Link href="#memes">Log in</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
