02849dfacf736af2119e2b8aeea6a09b558f81ef
khaled39ab <khaled39ab@gmail.com>
2022-06-26 23:46:21 +0600

    created login & register component

--- src/Pages/Others/NotFound/NotFound.js
+++ src/Pages/Others/NotFound/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFound from '../../../images/404.webp';
 
 const NotFound = () => {
     return (
-        <div className='text-center'>
-            <h1>404</h1>
-            <h2>Page Not Found</h2>
+        <div className='text-center w-100'>
+            <img src={notFound} alt="" />
+            <h2><Link to={'/'}>Go to Home</Link></h2>
         </div>
     );
 };
