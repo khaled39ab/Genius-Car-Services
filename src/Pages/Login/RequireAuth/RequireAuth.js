eb595d6221338d7cfb33abfb250a87d8aae64ac8
khaled39ab <khaled39ab@gmail.com>
2022-07-10 00:05:55 +0600

    sent verification email btn working

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
 const RequireAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
     const location = useLocation();
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(
+        auth
+    );
 
-    if (loading){
+    if (loading) {
         return <Loading></Loading>
     }
 
@@ -20,7 +24,11 @@ const RequireAuth = ({ children }) => {
         return <div className='text-center pt-5'>
             <h3 className='text-danger'>Verified Your Email First</h3>
             <h5 className='text-success'>Go to your email address and verify email</h5>
-            <p className='mt-3'>Did not get email? <button className='btn-link border-0 bg-white'>Sent verification email again</button></p>
+            <p className='mt-3'>Did not get email? <button onClick={async () => {
+                await sendEmailVerification();
+                toast('Sent email');
+            }} className='btn-link border-0 bg-white'>Sent verification email again</button></p>
+            <ToastContainer />
         </div>
     }
 
