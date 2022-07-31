0ac7edfa38a436576aa55bf10103938fa9e11325
khaled39ab <khaled39ab@gmail.com>
2022-07-31 23:54:53 +0600

    delete a specific service

--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -3,12 +3,28 @@ import useServices from '../../hooks/useSerivces';
 
 const ManageServices = () => {
     const [services, setServices] = useServices();
+
+    const handleDelete = id =>{
+        const proceed = window.confirm('Are you sure to delete?')
+        if(proceed){
+            const url = `http://localhost:5000/service/${id}`;
+            fetch(url, {
+                method:'DELETE',
+            })
+            .then (res => res.json())
+            .then (data => {
+                const remaining = services.filter( service =>service._id !== id)
+                setServices(remaining);
+            })
+        }
+    }
+
     return (
         <div>
             <h2>Manage your services</h2>
             {
-                services.map (service =><div key={service._id}>
-                    <h4>{service.name}</h4>
+                services.map (service =><div key={service._id} className="text-center mb-3">
+                    <h4>{service.name} <button onClick={ () => handleDelete (service._id)}>x</button></h4>
                 </div>)
             }
         </div>
