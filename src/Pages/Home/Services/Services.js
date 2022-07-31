2e164adf7ff4b29a8117bfd13a14b159b4419e49
khaled39ab <khaled39ab@gmail.com>
2022-07-31 23:45:14 +0600

    load services data from use a custom hook and create a manage page

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import useServices from '../../../hooks/useSerivces';
 import Service from '../Service/Service';
 import './Services.css'
 
 const Services = () => {
-    const [services, setServices] = useState([]);
-    useEffect(() => {
+    const [services, setServices] = useServices();
+    /* useEffect(() => {
         fetch('http://localhost:5000/service')
             .then(res => res.json())
             .then(data => setServices(data))
-    }, [])
+    }, []) */
 
     return (
         <div id='services' className='container mt-4'>
--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import useServices from '../../hooks/useSerivces';
 
 const ManageServices = () => {
+    const [services, setServices] = useServices();
     return (
         <div>
             <h2>Manage your services</h2>
+            {
+                services.map (service =><div key={service._id}>
+                    <h4>{service.name}</h4>
+                </div>)
+            }
         </div>
     );
 };
