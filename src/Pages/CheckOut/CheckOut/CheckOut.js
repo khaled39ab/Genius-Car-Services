69e21c50b838573da950283ed148d2926cbfb19a
khaled39ab <khaled39ab@gmail.com>
2022-08-11 00:30:43 +0600

    create a form to collect user order info

--- src/Pages/CheckOut/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut/CheckOut.js
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import useServiceDetail from '../../../hooks/useServiceDetail';
 
 const CheckOut = () => {
     const { serviceId } = useParams();
-    const [service, setService] = useServiceDetail(serviceId);
+    const [service] = useServiceDetail(serviceId);
+    const [user] = useAuthState(auth);
 
     return (
         <div>
             <h2>Please checkout {service.name}</h2>
+            <form className='w-50 mx-auto'>
+                <input className='w-100 mb-2' type="text" value={user.displayName} name='name' placeholder='Name' required readOnly disabled/>
+                <br />
+                <input className='w-100 mb-2' type="text" value={user.email} name='email' placeholder='Email' required readOnly disabled/>
+                <br />
+                <input className='w-100 mb-2' type="text" value={service.name} name='service-name' placeholder='Service Name' required readOnly disabled/>
+                <br />
+                <textarea className='w-100 mb-2' type="text" name='address' placeholder='Address' required />
+                <br />
+                <input className='w-100 mb-2' type="text" name='phone' placeholder='Phone' required autoComplete='off' />
+                <br />
+                <input className='w-100 btn btn-primary mb-5' type="submit" value="Order Now" />
+            </form>
         </div>
     );
 };
