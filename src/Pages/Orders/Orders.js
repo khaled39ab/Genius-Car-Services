08c7bf1e46a3745a815e9d6f3d31f519b297728c
khaled39ab <khaled39ab@gmail.com>
2022-08-12 22:46:54 +0600

    Handle 401, 403 to log out and navigate to login page a user

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -4,24 +4,36 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
 
 const Orders = () => {
     const [user] = useAuthState(auth);
     const [order, setOrder] = useState([]);
-    useEffect( () =>{
-        const getOrder = async () =>{
+    const navigate = useNavigate();
+    useEffect(() => {
+        const getOrder = async () => {
             const email = user.email;
             const url = `http://localhost:5000/order?email=${email}`;
-            const {data} = await axios.get(url, {
-                headers: {
-                    authorization : `bearer ${localStorage.getItem('accessToken')}`
+            try {
+                const { data } = await axios.get(url, {
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
+                setOrder(data);
+            }
+            catch (err) {
+                console.log(err.message);
+                if (err.response.status === 401 || err.response.status === 403) {
+                    signOut(auth)
+                    navigate('/login')
                 }
-            });
-            setOrder(data);
+            }
         }
         getOrder();
 
-    },[user])
+    }, [user, navigate])
 
     return (
         <div>
